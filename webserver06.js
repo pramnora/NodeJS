c6a84633899c807b3a48f559369e47713f4d41fa
Paul Ramnora <ceo@paulramnora.com>
2018-11-11 13:24:15 +0000

    Create webserver06.js
    NodeJS: Create a simple web server, and, output a 'Hello, world!' message.

---
+++ webserver06.js
@@ -0,0 +1,9 @@
+// Example taken from...
+// https://www.w3schools.com/nodejs/nodejs_get_started.asp
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080); 
