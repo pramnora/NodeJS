fde16a9da4447e91104c4cbb711cb325f16e240e
Paul Ramnora <pramnora@yahoo.com>
2018-11-11 13:50:23 +0000

    Update webserver07.js
    NodeJS: Added explanatory code comments

--- webserver07.js
+++ webserver07.js
@@ -1,10 +1,10 @@
 // Code borrowed from...
 // https://www.youtube.com/watch?v=GJmFG4ffJZU
 
-var http=require('http');
-http.createServer(function(request,response){
- response.writeHead(200);
- response.write("Hello, world!");
- response.end();
-}).listen(8080);
-console.log('Listening on port 8080...');
+var http=require('http');                      // how we require modules
+http.createServer(function(request,response){  // create server
+ response.writeHead(200);                      // status code in header
+ response.write("Hello, world!");              // response body
+ response.end();                               // close the connection
+}).listen(8080);                               // listen for connections on this port
+console.log('Listening on port 8080...');      // write output message to console while server is still running 
