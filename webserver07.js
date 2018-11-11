d054a982ac4925939f535e712c9f5c1cab79850f
Paul Ramnora <ceo@paulramnora.com>
2018-11-11 13:46:21 +0000

    Create webserver07.js
    NodeJS: Create web server: and, output: 'Hello, world!'

---
+++ webserver07.js
@@ -0,0 +1,10 @@
+// Code borrowed from...
+// https://www.youtube.com/watch?v=GJmFG4ffJZU
+
+var http=require('http');
+http.createServer(function(request,response){
+ response.writeHead(200);
+ response.write("Hello, world!");
+ response.end();
+}).listen(8080);
+console.log('Listening on port 8080...');
