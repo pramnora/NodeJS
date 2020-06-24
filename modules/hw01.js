4b3f281a5f04160e6208dfe54809e9c3cf117c23
Paul Ramnora <pramnora@yahoo.com>
2020-06-24 17:48:34 +0100

    Create hw01.js
    Initial commit. Created file: [hw01.js] / This is the module file [hw01.js] that will be called/loaded by: [call-module-01.js]; -(both files need to be stored inside of the same one directory folder in order to work)-;  used in conjunction together they print out the message: 'Hello, world!'

---
+++ modules/hw01.js
@@ -0,0 +1,10 @@
+/*
+ NOTE: This is the [hw01.js] module file...,
+       that is called by file: [call-module-01.js];
+       and, therefore, for both of these files to work, effectively;
+       they both need to be kept stored together inside of the one same folder directory. 
+*/       
+         
+exports.greetings = function (){
+  return "Hello, world!";
+}
