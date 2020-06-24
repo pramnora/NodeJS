9c800125483b941bc5f8a0dcebf4f91175f43f32
Paul Ramnora <pramnora@yahoo.com>
2020-06-24 19:44:07 +0100

    Update call-module-myCalculator.js
    Divided up the program to read more clearly...by adding code comment sections: Comment/Required/Variables/Main Program/-etc.

--- modules/call-module-myCalculator.js
+++ modules/call-module-myCalculator.js
@@ -1,11 +1,20 @@
-/* This file needs [myCalculator.js] to work...; and, both files should be kept stored together in the 'one' same folder directory. */
+/* Comments...
+ This file needs [myCalculator.js] to work...; and, both files should be kept stored together in the 'one' same folder directory.  
+ This program shows that it's possible to call any number of different functions...; being stored inside of one separate module file.    
+*/
 
-/* This program shows that it's possible to call any number of different functions...; being stored inside of one separate module file. */ 
+/*** Required files...
 
 var http = require('http');
 var doMaths = require('./myCalculator.js');
+
+/*** Variable declarations...
+
 var num1 = 10
 var num2 = 2
+
+/*** Main program...
+
 http.createServer(function (req,res){
  res.writeHead(200,{'Content-Type':'text/html'});
  res.write("The total of: " + num1 + " + " + num2 + " = " + doMaths.add(num1,num2));
