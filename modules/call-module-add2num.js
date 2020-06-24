319ad80b1aa7bb47399fc15133a5633900c137db
Paul Ramnora <pramnora@yahoo.com>
2020-06-24 18:39:00 +0100

    Create call-module-add2num.js
    Initial commit. Created file: [call-module-add2num.js] / This file calls the module: [add2num.js]...and, then, passes that module 2 numbers to add; the numbers are added up inside of the module/then, returned back to the calling file...the calling file, next, displays the output result.

---
+++ modules/call-module-add2num.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var doMaths = require('./add2num.js');
+
+var num1 = 1
+var num2 = 2
+
+http.createServer(function (req,res){
+ res.writeHead(200,{'Content-Type':'text/html'});
+ res.write("The total of: " + num1 + " + " + num2 + " = " + doMaths.add(num1,num2));
+ res.end();
+}).listen(8080);
