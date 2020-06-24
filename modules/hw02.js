08edde63ac2cf31dc8b5f287333a4cb84266ed0e
Paul Ramnora <pramnora@yahoo.com>
2020-06-24 18:18:15 +0100

    Update hw02.js
    Added comments at the top of the program...

--- modules/hw02.js
+++ modules/hw02.js
@@ -1,3 +1,9 @@
+/*
+ NOTE: This [hw02.js] file...is the called module file;
+       it works in conjunction with the calling file: [call-module-hw02.js];
+       also, both of these files need to be kept stored inside of the 'one' same directory folder to work.
+*/
+
 var textMsg = "";
 
 textMsg  = "<br>Line 1";
