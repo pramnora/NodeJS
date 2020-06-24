881429f70d84a48d305396274fda1662e71550d3
Paul Ramnora <pramnora@yahoo.com>
2020-06-24 17:39:48 +0100

    Create call-module-01.js
    Initial commit. Created file: [call-module-01.js] / This NodeJS program calls an external [.js] file module in order to do it's job...which is to output a text message, saying: 'Hello, world!"

---
+++ modules/call-module-01.js
@@ -0,0 +1,13 @@
+/* 
+ NOTE: This file requires the file: [hw01.js];
+       which is kept stored inside of the same folder directory to work.
+*/
+
+var http = require('http');
+var message = require('./hw01.js');
+
+http.createServer(function (req,res){
+ res.writeHead(200,{'Content-Type':'text/html'});
+ res.write("Message is: " + message.greetings());
+ res.end();
+}).listen(8080);
