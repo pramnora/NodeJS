cd12fd796235e5ff4c97f803a0a8522a7c20d9c2
Paul Ramnora <pramnora@yahoo.com>
2020-06-24 18:19:11 +0100

    Update call-module-hw02.js
    Changed: 'greetings' to say 'multiLineText'

--- modules/call-module-hw02.js
+++ modules/call-module-hw02.js
@@ -8,6 +8,6 @@ var message = require('./hw02.js');
 
 http.createServer(function (req,res){
  res.writeHead(200,{'Content-Type':'text/html'});
- res.write("Message is:<br>" + message.greetings());
+ res.write("Message is:<br>" + message.multiLineText());
  res.end();
 }).listen(8080);
