68af238f248828020b3e6d3bb87edef83a0989b8
Paul Ramnora <pramnora@yahoo.com>
2020-06-24 17:51:23 +0100

    Update hw01.js
    Changed comment filename to become: [call-module-hw01.js]

--- modules/hw01.js
+++ modules/hw01.js
@@ -1,6 +1,6 @@
 /*
  NOTE: This is the [hw01.js] module file...,
-       that is called by file: [call-module-01.js];
+       that is called by file: [call-module-hw01.js];
        and, therefore, for both of these files to work, effectively;
        they both need to be kept stored together inside of the one same folder directory. 
 */       
