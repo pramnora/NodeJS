bab51ff5763cbf1b58364bc7c59f7475aa67b1e8
Paul Ramnora <pramnora@yahoo.com>
2020-06-24 19:29:08 +0100

    Create myCalculator.js
    Initial commit. Created file: [myCalculator.js] / This file works in conjunction with it's caller: [call-module-myCalculator.js]

---
+++ modules/myCalculator.js
@@ -0,0 +1,15 @@
+exports.add = function (x,y){ 
+  return x+y;
+}
+exports.subtract = function (x,y){ 
+  return x-y;
+}
+exports.multiply = function (x,y){ 
+  return x*y;
+}
+exports.divide = function (x,y){ 
+  return x/y;
+}
+exports.exponent = function (x,y){ 
+  return x**y;
+}
