403bd2baf190ebd3e22186875639731680afd110
Paul Ramnora <pramnora@yahoo.com>
2020-11-30 05:43:57 +0000

    Create 12xtables-01.js
    Created: [12xtables-01.js]  / Prints out the selected times tables going from 1 x n...up to...12 x n

---
+++ language/math/12xtables-01.js
@@ -0,0 +1,7 @@
+var intTablesNo = 7; //...select which number times tables to have printed out
+
+//*** print out the selected number times tables...going from 1 x n... up to 12 x n...
+
+for (var intTimesNo = 1; intTimesNo <= 12; intTimesNo++){
+ console.log(intTimesNo + " X " + intTablesNo + " = " + intTimesNo * intTablesNo);
+}
