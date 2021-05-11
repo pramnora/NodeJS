916e497522c14c332852c32b4dbdb7f58b593d9d
Paul Ramnora <pramnora@yahoo.com>
2021-05-11 22:04:35 +0100

    Update write01e.js
    Added some commentary notes...

--- language/filing/write/sync/write01e.js
+++ language/filing/write/sync/write01e.js
@@ -1,3 +1,9 @@
+/* 
+ This program demonstrates how it's possible to use, NodeJS; 
+ to write a program file of a different programming language,
+ and, in this particular case, the language is: Python/[.py]
+*/
+
 const fileSystem = require("fs");
 var pythonCodeContent = "print('Hello, world!')"
 fileSystem.writeFileSync("hw01.py",pythonCodeContent);
