e2d1821b0c6ef851ef670698d7f9887858d30718
Paul Ramnora <pramnora@yahoo.com>
2021-05-11 22:06:04 +0100

    Update write01d.js
    Added commentary note...

--- language/filing/write/sync/write01d.js
+++ language/filing/write/sync/write01d.js
@@ -1,3 +1,9 @@
+/* 
+ This program demonstrates how it's possible to use, NodeJS; 
+ to write a program file of a different programming language,
+ and, in this particular case, that language is: HTML/[.html]
+*/
+
 const fileSystem = require("fs");
 var webPageContent = "<!DOCTYPE html>\n"
                    + "<html lang='en'>\n"
